9bbcdda45fa5ddf8386aeb15e2f1e08ec4f37d23
KomalKhan776 <kkhan17@my.gcu.edu>
2026-01-18 15:17:53 -0700

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# 1. current date and time
+echo "Date and time:"
+date
+
+
+# 2. current working directory
+echo "Current directory:"
+pwd
+
+
+# 3. list of files in current directory
+echo "Files in current directory:"
+ls -l
+
+
+# 4. disk usage
+echo "Disk usage:"
+df -h
+
+
+# 5. currently login user
+echo "Login user:"
+who
